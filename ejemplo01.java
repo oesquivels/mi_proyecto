1de8233c7de0a151c373240ed3f0b68108eea96c
omes <omes@omesPC>
2016-09-05 04:03:04 -0500

    mi_proyecto: Primeros archivos subidos a github.com

---
+++ ejemplo01.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*OMES: 05/09/2016
+/**
+ *
+ * @author omes
+ */
+public class ejemplo01 {
+    
+}
